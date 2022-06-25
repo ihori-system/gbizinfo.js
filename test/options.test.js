6c70cdf6aae7c4ad8b887145ede874f334c9e0e8
Issei Horie <issei.horie@is2ei.com>
2022-06-25 22:01:56 +0900

    npm init

---
+++ test/options.test.js
@@ -0,0 +1,14 @@
+const {GbizinfoClient} = require('..');
+
+test('throws without option', () => {
+  const options = undefined;
+  expect(() => new GbizinfoClient(options)).toThrow();
+});
+
+test('throws without `token` option', () => {
+  expect(() => new GbizinfoClient({})).toThrow();
+});
+
+test('not to throw with `token` option', () => {
+  expect(() => new GbizinfoClient({token: 'xxxxx'})).not.toThrow();
+});
